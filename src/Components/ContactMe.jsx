50ccaf0ac060d6562102251a57a8b022d04e3820
Webby1015 <vedantabhishek@gmail.com>
2024-03-18 18:03:10 +0530

    nice

--- src/Components/ContactMe.jsx
+++ src/Components/ContactMe.jsx
@@ -1,42 +1,70 @@
-import React from 'react';
+import React from "react";
 
 const ContactMe = () => {
   const gradientStyle = {
-    background: 'linear-gradient(to bottom, #F0F8FF, white)',
+    background: "linear-gradient(to bottom, #F0F8FF, white)",
   };
 
   return (
-    <div id='Contact' >
-    <div  className="h-screen flex flex-col justify-center items-center" style={gradientStyle}>
-    <h2 className="text-5xl font-bold mb-10 text-gray-900 leading-tight ">
-        <span className="">Contact </span><span className="font-thin bg-violet-300 rounded-full">Me</span>
-      </h2>
-      <p className="text-lg mb-4 text-center">Feel free to reach out to me:</p>
-      <div class="bg-gray-200 p-10 rounded-md shadow-inner">
-  <div class="flex flex-row flex-wrap justify-center">
-    <div class="w-full md:w-1/2 lg:w-1/3 xl:w-1/4 p-4">
-      <div class="bg-blue-200 shadow-md rounded px-6 py-8">
-        <p class="text-lg">Email: <a href="mailto:example@example.com" class="text-blue-700">Write an Email</a></p>
+    <div id="Contact">
+      <div
+        className="h-screen flex flex-col justify-center items-center"
+        style={gradientStyle}
+      >
+        <h2 className="text-5xl font-bold mb-10 text-gray-900 leading-tight ">
+          <span className="">Contact </span>
+          <span className="font-thin bg-violet-300 rounded-full">Me</span>
+        </h2>
+        <p className="text-lg mb-4 text-center">
+          Feel free to reach out to me:
+        </p>
+        <div class="bg-gray-200 lg:p-10 w-9/12 lg:mb-10 rounded-md shadow-inner">
+          <div class="flex flex-row flex-wrap justify-center">
+            <div class="w-full md:w-1/2 lg:w-1/2 xl:w-1/4 p-4">
+              <div class="bg-blue-200 shadow-md rounded px-6 py-8">
+                <p class="text-lg">
+                  Email:{" "}
+                  <a href="mailto:example@example.com" class="text-blue-700">
+                    Write an Email
+                  </a>
+                </p>
+              </div>
+            </div>
+            <div class="w-full md:w-1/2 lg:w-1/3 xl:w-1/4 p-4">
+              <div class="bg-green-200 shadow-md rounded px-6 py-8">
+                <p class="text-lg">
+                  Phone:{" "}
+                  <a href="tel:+1234567890" class="text-green-700">
+                    +1234567890
+                  </a>
+                </p>
+              </div>
+            </div>
+            <div class="w-full md:w-1/2 lg:w-1/3 xl:w-1/4 p-4">
+              <div class="bg-yellow-200 shadow-md rounded px-6 py-8">
+                <p class="text-lg">
+                  Linkedin:{" "}
+                  <a href="#" class="text-yellow-700">
+                    View Profile
+                  </a>
+                </p>
+              </div>
+            </div>
+            <div class="w-full md:w-1/2 lg:w-1/3 xl:w-1/4 p-4">
+              <div class="bg-red-200 shadow-md rounded px-6 py-8">
+                <p class="text-lg">
+                  Resume:{" "}
+                  <a href="#" class="text-red-700">
+                    Download
+                  </a>
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
-    </div>
-    <div class="w-full md:w-1/2 lg:w-1/3 xl:w-1/4 p-4">
-      <div class="bg-green-200 shadow-md rounded px-6 py-8">
-        <p class="text-lg">Phone: <a href="tel:+1234567890" class="text-green-700">+1234567890</a></p>
-      </div>
-    </div>
-    <div class="w-full md:w-1/2 lg:w-1/3 xl:w-1/4 p-4">
-      <div class="bg-yellow-200 shadow-md rounded px-6 py-8">
-        <p class="text-lg">Linkedin: <a href="#" class="text-yellow-700">View Profile</a></p>
-      </div>
-    </div>
-  </div>
-</div>
-
-
-    </div>
-    
     </div>
   );
-}
+};
 
 export default ContactMe;
--- src/Config/firebase.js
+++ src/Config/firebase.js
@@ -1,22 +1,21 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
-import {getStorage} from "firebase/storage"
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyBRuQIidrNS4OLq3bAGzXF6qHZXocYvxqI",
-  authDomain: "prakhar-portfolio-ce872.firebaseapp.com",
-  projectId: "prakhar-portfolio-ce872",
-  storageBucket: "prakhar-portfolio-ce872.appspot.com",
-  messagingSenderId: "741567686623",
-  appId: "1:741567686623:web:3f74c94875a7517e42466a",
-  measurementId: "G-6TNV37C9JL"
+  apiKey: import.meta.env.VITE_API_KEY,
+  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_APP_ID,
+  measurementId: import.meta.env.VITE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
+export const auth = getAuth(app);
